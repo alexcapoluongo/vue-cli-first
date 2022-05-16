cab3e51d959e1cf8b3529afc2b72215f94250acb
alexcapoluongo <alexcapoluongo@icloud.com>
2022-05-16 13:54:55 +0200

    HelloWorld item removed

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,15 @@
 <template>
   <div id="app">
     <Title />
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
 import Title from "./components/Title.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
     Title,
   },
 };
